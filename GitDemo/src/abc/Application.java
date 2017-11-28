893723f8cee6680c761a8cbaa8f0bc41c15a7a95
bneupane <neupanebabu828@gmail.com>
2017-11-28 14:05:04 +0545

    new line added

--- GitDemo/src/abc/Application.java
+++ GitDemo/src/abc/Application.java
@@ -3,5 +3,6 @@ package abc;
 public class Application {
     public static void main(String[] args) {
         System.out.println("hello from the application");
+        System.out.println("new line added");
     }
 }
